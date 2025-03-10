946089f7c18faca5a6c9d4e761eb536da9266781
Luca Knaack <l.knaack93@gmail.com>
2025-03-10 22:13:08 +0000

    log in backend

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,11 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-      origin: 'http://nip.kncklab.com', // Frontend domain
-      methods: ['GET', 'POST']
-    }
-  });
+  cors: {
+    origin: 'http://nip.kncklab.com', // Frontend domain
+    methods: ['GET', 'POST']
+  }
+});
 
 app.use(express.static('public'));
 
@@ -20,18 +20,24 @@ let sentences = {};
 let gameState = 'waiting'; // waiting, playing, stopped
 
 io.on('connection', (socket) => {
+  console.log('New client connected:', socket.id); // Debug log
+
   socket.on('login', (data) => {
+    console.log('Login attempt:', data); // Debug log
     if (data.password === PASSWORD && data.name && !users.includes(data.name)) {
       users.push(data.name);
       socket.name = data.name;
+      console.log('Login success for:', data.name); // Debug log
       socket.emit('loginSuccess', { users, gameMaster: users[currentGameMasterIndex] });
       io.emit('updateUsers', { users, gameMaster: users[currentGameMasterIndex] });
     } else {
+      console.log('Login failed for:', data.name); // Debug log
       socket.emit('loginFailed');
     }
   });
 
   socket.on('submitSentence', (sentence) => {
+    console.log('Sentence submitted:', { name: socket.name, sentence }); // Debug log
     if (gameState === 'playing' && socket.name !== users[currentGameMasterIndex]) {
       sentences[socket.name] = sentence;
       io.to(users[currentGameMasterIndex]).emit('newSentence', { name: socket.name, sentence });
@@ -39,6 +45,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('stopGame', () => {
+    console.log('Stop game requested by:', socket.name); // Debug log
     if (socket.name === users[currentGameMasterIndex] && gameState === 'playing') {
       gameState = 'stopped';
       io.emit('gameStopped');
@@ -46,6 +53,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('doneGame', () => {
+    console.log('Done game requested by:', socket.name); // Debug log
     if (socket.name === users[currentGameMasterIndex] && gameState === 'stopped') {
       currentGameMasterIndex = (currentGameMasterIndex + 1) % users.length;
       sentences = {};
@@ -55,6 +63,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.name); // Debug log
     if (socket.name) {
       users = users.filter(u => u !== socket.name);
       if (users.length > 0) {
